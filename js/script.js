1b0ab3544e1eaba1ef02cd848ac10f269490bda2
Ruben Ponce <ruben_ponce1@yahoo.com>
2018-10-23 21:37:37 -0700

    added clarity and removed unneccesary comments

--- js/script.js
+++ js/script.js
@@ -8,12 +8,7 @@ let ul = document.querySelector(".student-list");
 let li= document.querySelectorAll(".student-item");
 let div = document.querySelector(".page");
 const listLength = 10;
-let remainder;
-  if(li.length%2!=0){
-    remainder = 1;
-  } else{
-    remainder = 2;
-  }
+
 
 // Create a function to hide all of the items in the list excpet for the ten you want to show
 // Tip: Keep in mind that with a list of 54 studetns, the last page will only display four
@@ -31,28 +26,30 @@ function showPage(listLength, page){
       li[i].style.display ="block";
   }
 }
-console.log((li.length-4) % 2);
-// Create and append the pagination links - Creating a function that can do this is a good approach
-//create's buttons for page class depending on total list length
-let buttonDiv = document.createElement('div');
-buttonDiv.className = "pagination";
-div.appendChild(buttonDiv);
+//begin appending links
+  let linkDiv = document.createElement('div');
+  linkDiv.className = "pagination";
+  div.appendChild(linkDiv);
 
-//initialize remainder for the case of a li.length perfectly divisible by listLength
+  let remainder;
+    if(li.length%2!=0){
+      remainder = 1;
+    } else{
+      remainder = 2;
+    }
+  //li.length/listLength of index 1 iterates through n-1 times. + remainder to iterate through list interval AND for any remainder.
+  for (let i = 1; i < Math.floor( li.length/listLength)+remainder; i++) {
 
-//li.length/listLength of index 1 iterates through n-1 times. + remainder to iterate through list interval AND for any remainder.
-for (let i = 1; i < Math.floor( li.length/listLength)+remainder; i++) {
-
-  //append list to div and anchor to list.
-  let paginationList = document.createElement('li');
-  buttonDiv.appendChild(paginationList);
-  let paginationLink = document.createElement('a');
-  paginationList.appendChild(paginationLink);
-  paginationLink.textContent = (i).toString();
-  paginationLink.addEventListener('click',(event)=>{
-    showPage(10, i);
-  });
-}
+    //append list to div and anchor to list.
+    let paginationList = document.createElement('li');
+    linkDiv.appendChild(paginationList);
+    let paginationLink = document.createElement('a');
+    paginationList.appendChild(paginationLink);
+    paginationLink.textContent = (i).toString();
+    paginationLink.addEventListener('click',()=>{
+      showPage(10, i);
+    });
+  }
 
 
 
