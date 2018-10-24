71520689870aa5af359cdd27e612c9f43162fb71
Ruben Ponce <ruben_ponce1@yahoo.com>
2018-10-23 21:32:58 -0700

    added case where li.length is perfectly divisible by list requirements

--- js/script.js
+++ js/script.js
@@ -8,13 +8,19 @@ let ul = document.querySelector(".student-list");
 let li= document.querySelectorAll(".student-item");
 let div = document.querySelector(".page");
 const listLength = 10;
+let remainder;
+  if(li.length%2!=0){
+    remainder = 1;
+  } else{
+    remainder = 2;
+  }
 
 // Create a function to hide all of the items in the list excpet for the ten you want to show
 // Tip: Keep in mind that with a list of 54 studetns, the last page will only display four
 
 function showPage(listLength, page){
   for (let i = 0; i < li.length; i++) {//this will iterate thru the entire list
-    let domainStart = listLength*page-listLength;//(show page(10, 1), domainStart = 0)
+    let domainStart = listLength*page-listLength;//(showPage(10, 1), domainStart = 0)
       if(i<domainStart){
         li[i].style.display = "none";
       }
@@ -25,14 +31,17 @@ function showPage(listLength, page){
       li[i].style.display ="block";
   }
 }
-
+console.log((li.length-4) % 2);
 // Create and append the pagination links - Creating a function that can do this is a good approach
 //create's buttons for page class depending on total list length
 let buttonDiv = document.createElement('div');
 buttonDiv.className = "pagination";
 div.appendChild(buttonDiv);
-//li.length/listLength of index 1 iterates through n-1 times. + 1 to iterate through list interval AND + 1 for any remainder.
-for (let i = 1; i < Math.floor( li.length/listLength)+2; i++) {
+
+//initialize remainder for the case of a li.length perfectly divisible by listLength
+
+//li.length/listLength of index 1 iterates through n-1 times. + remainder to iterate through list interval AND for any remainder.
+for (let i = 1; i < Math.floor( li.length/listLength)+remainder; i++) {
 
   //append list to div and anchor to list.
   let paginationList = document.createElement('li');
