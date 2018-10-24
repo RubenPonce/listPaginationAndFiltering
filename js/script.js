f250c792c1eb4f39d60da04a411385b9232bdf4f
Ruben Ponce <ruben_ponce1@yahoo.com>
2018-10-23 19:47:07 -0700

    changed buttons to links but kept same functionality

--- js/script.js
+++ js/script.js
@@ -3,13 +3,15 @@ Treehouse Techdegree:
 FSJS project 2 - List Filter and Pagination
 ******************************************/
 // Add variables that store DOM elements you will need to reference and/or manipulate
+
+
 let ul = document.querySelector(".student-list");
 let li= document.querySelectorAll(".student-item");
-let div = document.querySelector(".page")
+let div = document.querySelector(".page");
 const listLength = 10;
 
 
-console.log("test");
+
 
 // Create a function to hide all of the items in the list excpet for the ten you want to show
 // Tip: Keep in mind that with a list of 54 studetns, the last page will only display four
@@ -32,18 +34,23 @@ function showPage(listLength, page){
 
 // Create and append the pagination links - Creating a function that can do this is a good approach
 //create's buttons for page class depending on total list length
-
+let buttonDiv = document.createElement('div');
+buttonDiv.className = "pagination";
+div.appendChild(buttonDiv);
+//li.length/listLength of index 1 iterates through n-1 times. + 1 to iterate through list interval AND + 1 for any remainder.
 for (let i = 1; i < Math.floor( li.length/listLength)+2; i++) {
-  let button = document.createElement('button');
-  div.appendChild(button);
-  button.textContent = (i).toString();
-  button.className = "pageButtons";
-  button.addEventListener('click',(event)=>{
-    if(event.target.tagName === "BUTTON"){
+
+  //append list to div and anchor to list.
+  let paginationList = document.createElement('li');
+  buttonDiv.appendChild(paginationList);
+  let paginationLink = document.createElement('a');
+  paginationList.appendChild(paginationLink);
+  paginationLink.textContent = (i).toString();
+  paginationLink.addEventListener('click',(event)=>{
     showPage(10, i);
-    }
   });
 }
+
 
 
 
