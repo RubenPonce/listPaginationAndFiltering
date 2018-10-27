52e0c4a7a1a570d7eacf13c33ec0dd3f8c23e8b8
Ruben Ponce <ruben_ponce1@yahoo.com>
2018-10-27 14:35:21 -0700

    added active anchor tag to anchor's clicked. will try to add search capability next

---
+++ js/script.js
@@ -0,0 +1,66 @@
+/******************************************
+Treehouse Techdegree:
+FSJS project 2 - List Filter and Pagination
+******************************************/
+// Add variables that store DOM elements you will need to reference and/or manipulate
+
+let ul = document.querySelector(".student-list");
+let li= document.querySelectorAll(".student-item");
+let div = document.querySelector(".page");
+
+
+
+// Create a function to hide all of the items in the list excpet for the ten you want to show
+// Tip: Keep in mind that with a list of 54 studetns, the last page will only display four
+
+function showPage(list, page){
+  for (let i = 0; i < li.length; i++) {//this will iterate thru the entire list
+    let domainStart = list*page-list;//(showPage(10, 1), domainStart = 0)
+      if(i<domainStart){
+        li[i].style.display = "none";
+      }
+      if(i>= domainStart+list){
+        li[i].style.display = "none";
+      }
+      if(i>=domainStart && i<domainStart+list)
+      li[i].style.display ="block";
+  }
+}
+
+let appendLinks = (list) => {
+  //append paginationDiv to div and ul to paginationDiv, and list to ul, anchor to list.
+  let paginationDiv = document.createElement('div');
+    if(paginationDiv.className ==="pagination"){
+      paginationDiv.className = "";
+    }
+  paginationDiv.className = "pagination";
+  let pageUl = document.createElement('ul');
+  div.appendChild(paginationDiv);
+  paginationDiv.appendChild(pageUl);
+  for (var i = 0; i < li.length/list; i++) {
+
+    let paginationList = document.createElement('li');
+    let anchor = document.createElement('a');
+    pageUl.appendChild(paginationList);
+    paginationList.appendChild(anchor);
+    anchor.textContent = (i+1).toString();
+    anchor.href = "#";
+    if(i === 0){
+      anchor.className = "active";
+    }
+    anchor.addEventListener('click', (event) =>{
+
+      let anchorName = document.querySelector(".active");
+      if(anchorName !==null){
+        anchorName.classList.remove("active");
+      }
+      event.target.className = "active";
+      showPage(10, parseInt(event.target.textContent));
+    });
+
+
+  }//end for-loop
+
+} //end appendLinks
+appendLinks(10);
+showPage(10, 1);
